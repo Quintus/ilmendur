b050ca9b48d96c1958b9334604618b2b18421d37
Marvin Gülker <post+git@guelker.eu>
2020-12-30 22:10:12 +0100

    Replace text-based hello-world with one that actually shows a window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* p_window = glfwCreateWindow(640, 480, "Hello world", nullptr, nullptr);
+    if (!p_window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(p_window);
+
+    while (!glfwWindowShouldClose(p_window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(p_window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
