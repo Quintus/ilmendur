ca81e4c3713a3190f3f56475f29a3d2f08a3550d
Marvin Gülker <post+git@guelker.eu>
2022-02-27 16:27:59 +0100

    Use a simple cube for the player figure for the moment.

--- src/actors/freya.cpp
+++ src/actors/freya.cpp
@@ -7,7 +7,9 @@ Freya::Freya(SceneSystem::Scene& scene)
 {
     m_mass = 0.63f;
     m_colltype = PhysicsSystem::ColliderType::box;
-    mp_scene_node->attachObject(m_scene.getSceneManager().createEntity("sign.mesh"));
+    //mp_scene_node->attachObject(m_scene.getSceneManager().createEntity("sign.mesh"));
+    mp_scene_node->attachObject(m_scene.getSceneManager().createEntity(Ogre::SceneManager::PrefabType::PT_CUBE));
+    mp_scene_node->setScale(0.01, 0.01, 0.01);
 }
 
 Freya::~Freya()
