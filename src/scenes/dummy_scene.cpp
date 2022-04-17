b715a7fb3ed3c802532b5e8f90ba5fd9462a8b7b
Marvin Gülker <post+git@guelker.eu>
2022-04-17 14:48:59 +0200

    Revert "Try to use nuklear UI library"
    This reverts commit f84e87ea654ee900fb06f88d08288e0f4075c291.

--- src/scenes/dummy_scene.cpp
+++ src/scenes/dummy_scene.cpp
@@ -113,35 +113,8 @@ void DummyScene::update()
     handleJoyInput();
 }
 
-enum {EASY, HARD};
 void DummyScene::draw()
 {
-    int op = EASY;
-    float value = 0.6f;
-    int i =  20;
-    nk_context& ctx = mp_ui_system->getContext();
-    if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220),
-                 NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE)) {
-        // fixed widget pixel width
-        nk_layout_row_static(&ctx, 30, 80, 1);
-        if (nk_button_label(&ctx, "button")) {
-            // event handling
-        }
-        // fixed widget window ratio width
-        nk_layout_row_dynamic(&ctx, 30, 2);
-        if (nk_option_label(&ctx, "easy", op == EASY)) op = EASY;
-        if (nk_option_label(&ctx, "hard", op == HARD)) op = HARD;
-        // custom widget pixel width
-        nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
-        {
-            nk_layout_row_push(&ctx, 50);
-            nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
-            nk_layout_row_push(&ctx, 110);
-            nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
-        }
-        nk_layout_row_end(&ctx);
-    }
-    nk_end(&ctx);
     mp_ui_system->draw();
 }
 
