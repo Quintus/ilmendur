de771c30fa5640214eb792d050d10f5019a39dc3
Marvin Gülker <post+git@guelker.eu>
2022-08-02 21:14:27 +0200

    Add some administrative files

---
+++ .dir-locals.el
@@ -0,0 +1,17 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil .
+      ((indent-tabs-mode . nil)
+       (tab-width . 4)
+       (show-trailing-whitespace . t)))
+ (c-mode .
+         ((c-file-style . "k&r")
+          (c-basic-offset . 4)))
+ (c++-mode .
+	       ((c-file-style . "stroustrup")
+	        (c-basic-offset . 4)
+	        (show-trailing-whitespace . t)))
+ (nxml-mode .
+            ((nxml-child-indent . 4)
+             (nxml-attribute-indent . 4))))
