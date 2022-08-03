95a1c7d70d76ab6c1a005a73173a8acf6eb8ee2f
Marvin Gülker <post+git@guelker.eu>
2022-08-03 10:38:17 +0200

    Add some game data

---
+++ data/tilesets/Forst Town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Forst Town" tilewidth="32" tileheight="32" tilecount="872" columns="8">
+ <image source="forest_town.png" width="256" height="3507"/>
+</tileset>
---
+++ data/tilesets/Steampunk Town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Steampunk Town" tilewidth="32" tileheight="32" tilecount="632" columns="8">
+ <image source="steampunk_town.png" width="256" height="2528"/>
+</tileset>
