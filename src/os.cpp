a1c953a30c018158df42e1729904513c12cc0a28
Marvin Gülker <post+git@guelker.eu>
2022-08-03 10:38:33 +0200

    Enhance OS basics

--- src/os.cpp
+++ src/os.cpp
@@ -1,14 +1,24 @@
 #include "os.hpp"
 #include "buildconfig.hpp"
 
+#if defined(_WIN32)
+#include <Windows.h>
+#elif defined(__unix__)
+#include <cstdlib>
+#include <unistd.h>
+#include <limits.h>
+#else
+#error Unsupported system
+#endif
+
 using namespace std;
 namespace fs = std::filesystem;
 
-fs::path OS::game_data_dir()
+fs::path OS::gameDataDir()
 {
 #ifdef ILMENDUR_DEBUG_BUILD
     // Support running from the build directory in debug mode
-    if (fs::exists(OS::exe_path().parent_path() / fs::u8path("CMakeCache.txt"))) {
+    if (fs::exists(OS::exePath().parent_path() / fs::u8path("CMakeCache.txt"))) {
         return fs::u8path(ILMENDUR_SOURCE_DIR) / fs::u8path("data");
     } else {
 #endif
@@ -17,4 +27,24 @@ fs::path OS::game_data_dir()
     }
 #endif
     return fs::path(ILMENDUR_DATADIR);
+}
+
+fs::path OS::exePath()
+{
+#if defined(__linux__)
+    char buf[PATH_MAX];
+    ssize_t size = readlink("/proc/self/exe", buf, PATH_MAX); // This file contains native encoding, whcih fits fs::path constructfor for "char"
+    if (size < 0)
+        throw(runtime_error("Failed to read /proc/self/exe"));
+    return fs::path(string(buf, size));
+#elif defined(_WIN32)
+    wchar_t buf[MAX_PATH];
+    if (GetModuleFileNameW(NULL, buf, MAX_PATH) == 0) {
+        throw(runtime_error(winerror(GetLastError())));
+    }
+
+    return fs::path(buf); // Windows API returns native wide encoding, which fits fs::path constructor for "wchar_t"
+#else
+#error Unsupported platform
+#endif
 }
--- src/os.hpp
+++ src/os.hpp
@@ -1,5 +1,13 @@
+#ifndef ILMENDUR_OS_HPP
+#define ILMENDUR_OS_HPP
+
+#include <string>
 #include <filesystem>
 
 namespace OS {
-    std::filesystem::path game_data_dir();
+    std::filesystem::path exePath();
+
+    std::filesystem::path gameDataDir();
 }
+
+#endif /* ILMENDUR_OS_HPP */
